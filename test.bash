1e36e8cc82aae147bbf0fc3b4e2c6e258b2f441a
Keiichiro Kobayashi <s23c1050eb@s.chibakoudai.jp>
2024-10-31 12:46:06 +0900

    add Testcode

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,9 +6,17 @@ ng () {
 }
 
 res=0
-
+### NORMAL INPUT###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+### STRANGE INPUT###
+out =$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 
